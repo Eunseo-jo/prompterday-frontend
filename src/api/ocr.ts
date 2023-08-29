b9054ff417887d126c7766c751efc5dbef82decb
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-08-30 04:15:51 +0900

    test: ocr api test

--- src/api/ocr.ts
+++ src/api/ocr.ts
@@ -14,7 +14,7 @@ export const requestOCR = async ({
 }: RequestOCR) => {
   try {
     const response = await axios.post<ResponseOCR>(
-      `${process.env.REACT_APP_OCR_API_GATEWAY}`,
+      'https://e7nd31pts4.apigw.ntruss.com/custom/v1/24573/3e2b65f56cdc69bd54e9541a95a4d31136c3158761727780ee46061372e92766/general',
       {
         version: 'V2',
         requestId: 'id',
